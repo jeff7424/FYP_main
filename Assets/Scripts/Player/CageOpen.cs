446b36d7d151e911e4cfafcd5d924a4dccccca13
unknown <naomichim@M625GD01.sidm.student.local>
2015-10-06 11:07:08 +0800

    OpenCage

---
+++ Assets/Scripts/Player/CageOpen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageOpen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int id = Animator.StringToHash ("Open");
+		GetComponent<Animator>().SetBool(id, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay(Collider coll)
+	{
+		if (coll.tag == "Player" && Input.GetButtonDown ("Interact"))
+		{
+			Open ();
+		}
+	}
+
+	public void Open()
+	{
+		int id = Animator.StringToHash ("Open");
+		GetComponent<Animator>().SetBool(id, true);
+	}
+}
